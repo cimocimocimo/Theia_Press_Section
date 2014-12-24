83a58e27fb6e7c9bca53db557777f6a389fc0737
Aaron Cimolini <aaron@cimolini.com>
2014-12-24 14:50:59 -0500

    Trying to fix error on deploy.

--- scripts/create_admin.py
+++ scripts/create_admin.py
@@ -1,13 +1,15 @@
 #!/usr/bin/env python
 
-import sys
-# required now to setup the environment
-import django
-django.setup()
+try: 
 
-from django.contrib.auth.models import User
-if User.objects.count() == 0:
-    admin = User.objects.create_superuser('admin', 'aaron@cimolini.com', 'admin')
-    admin.save()
+    import sys
+    # required now to setup the environment
+    import django
+    django.setup()
+
+    from django.contrib.auth.models import User
+    if User.objects.count() == 0:
+        admin = User.objects.create_superuser('admin', 'aaron@cimolini.com', 'admin')
+        admin.save()
 
 sys.exit()
