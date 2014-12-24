26abe31a7672a5975078b11b9854279f75f093ce
Aaron Cimolini <aaron@cimolini.com>
2014-12-24 15:19:06 -0500

    typo fix...

--- scripts/create_admin.py
+++ scripts/create_admin.py
@@ -2,15 +2,15 @@
 
 import sys
 
-# try: 
+try: 
 
-    # # required now to setup the environment
-    # import django
-    # django.setup()
+    # required now to setup the environment
+    import django
+    django.setup()
 
-    # from django.contrib.auth.models import User
-    # if User.objects.count() == 0:
-    #     admin = User.objects.create_superuser('admin', 'aaron@cimolini.com', 'admin')
-    #     admin.save()
+    from django.contrib.auth.models import User
+    if User.objects.count() == 0:
+        admin = User.objects.create_superuser('admin', 'aaron@cimolini.com', 'admin')
+        admin.save()
 
 sys.exit()
