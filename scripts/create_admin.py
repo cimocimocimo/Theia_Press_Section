7e407b82b1e6a618c4ae6ecb08f3bdfb6b6405f3
Aaron Cimolini <aaron@cimolini.com>
2014-12-24 12:47:38 -0500

    Adds a fix for teh create admin script.

--- scripts/create_admin.py
+++ scripts/create_admin.py
@@ -1,5 +1,6 @@
 #!/usr/bin/env python
 
+import sys
 # required now to setup the environment
 import django
 django.setup()
@@ -8,3 +9,5 @@ from django.contrib.auth.models import User
 if User.objects.count() == 0:
     admin = User.objects.create_superuser('admin', 'aaron@cimolini.com', 'admin')
     admin.save()
+
+sys.exit()
