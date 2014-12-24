5ac564c99c4181f1557cfd0acbceace63b32c307
Aaron Cimolini <aaron@cimolini.com>
2014-12-24 02:03:53 -0500

    This is required to get the superuser setup.

--- scripts/create_admin.py
+++ scripts/create_admin.py
@@ -1,5 +1,9 @@
 #!/usr/bin/env python
 
+# required now to setup the environment
+import django
+django.setup()
+
 from django.contrib.auth.models import User
 if User.objects.count() == 0:
     admin = User.objects.create_superuser('admin', 'aaron@cimolini.com', 'admin')
