47085772bb09f167009f9b27757efa973efd34b2
Aaron Cimolini <aaron@cimolini.com>
2015-01-01 08:32:42 -0500

    Fixes migration bug, missing config. Also adds install of sass on install.

--- press_section/settings.py
+++ press_section/settings.py
@@ -241,6 +241,7 @@ MIGRATION_MODULES = {
     'filer': 'filer.migrations_django',
     'cmsplugin_filer_file': 'cmsplugin_filer_file.migrations_django',
     'cmsplugin_filer_folder': 'cmsplugin_filer_folder.migrations_django',
+    'cmsplugin_filer_link': 'cmsplugin_filer_link.migrations_django',
     'cmsplugin_filer_image': 'cmsplugin_filer_image.migrations_django',
     'cmsplugin_filer_teaser': 'cmsplugin_filer_teaser.migrations_django',
     'cmsplugin_filer_video': 'cmsplugin_filer_video.migrations_django',
