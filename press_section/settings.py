29b77df7489dc55daa82bfd46f459676031406af
Aaron Cimolini <aaron@cimolini.com>
2015-01-03 09:26:39 -0500

    Allows all cors requests

--- press_section/settings.py
+++ press_section/settings.py
@@ -291,6 +291,8 @@ THUMBNAIL_DEFAULT_STORAGE = 'storages.backends.s3boto.S3BotoStorage'
 #     'Cache-Control': 'max-age=86400',
 # }
 
+CORS_ORIGIN_ALLOW_ALL = True
+
 CORS_ORIGIN_WHITELIST = (
     'theia-press-section-assets.s3.amazonaws.com',
 )
