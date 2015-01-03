6b0281471ef0553717cff105e9f91685b4d4676c
Aaron Cimolini <aaron@cimolini.com>
2015-01-03 04:36:49 -0500

    settitngs test

--- press_section/settings.py
+++ press_section/settings.py
@@ -277,10 +277,10 @@ AWS_ACCESS_KEY_ID = os.environ['AWS_ACCESS_KEY_ID']
 AWS_SECRET_KEY = os.environ['AWS_SECRET_KEY']
 AWS_STORAGE_BUCKET_NAME = 'theia-press-section-assets'
 # see http://developer.yahoo.com/performance/rules.html#expires
-AWS_HEADERS = {
-    'Expires': 'Thu, 15 Apr 2010 20:00:00 GMT',
-    'Cache-Control': 'max-age=86400',
-}
+# AWS_HEADERS = {
+#     'Expires': 'Thu, 15 Apr 2010 20:00:00 GMT',
+#     'Cache-Control': 'max-age=86400',
+# }
 
 # Local Development settings
 try:
