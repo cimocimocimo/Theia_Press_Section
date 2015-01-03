03b5dce89a551752585c7d97fc0fbfd58baa412a
Aaron Cimolini <aaron@cimolini.com>
2015-01-03 09:04:56 -0500

    boto fix

--- press_section/storage.py
+++ press_section/storage.py
@@ -1,5 +1,7 @@
 from django.core.files.storage import get_storage_class
 from storages.backends.s3boto import S3BotoStorage
+import urllib
+import urlparse
 
 class CachedS3BotoStorage(S3BotoStorage):
     """
@@ -9,10 +11,11 @@ class CachedS3BotoStorage(S3BotoStorage):
     querystring_auth=False
 
     def __init__(self, *args, **kwargs):
+
+        # merge in any arguments that were passed
+        kwargs.update(dict(location='public', querystring_auth=False))
+
         super(CachedS3BotoStorage, self).__init__(*args, **kwargs)
-
-        self.querystring_auth=False
-
         self.local_storage = get_storage_class(
             "compressor.storage.CompressorFileStorage")()
 
@@ -20,3 +23,12 @@ class CachedS3BotoStorage(S3BotoStorage):
         name = super(CachedS3BotoStorage, self).save(name, content)
         self.local_storage._save(name, content)
         return name
+
+    def url(self, name):
+        orig = super(CachedS3BotoStorage, self).url(name)
+        scheme, netloc, path, params, query, fragment = urlparse.urlparse(orig)
+        params = urlparse.parse_qs(query)
+        if 'x-amz-security-token' in params:
+            del params['x-amz-security-token']
+        query = urllib.urlencode(params)
+        return urlparse.urlunparse((scheme, netloc, path, params, query, fragment))
