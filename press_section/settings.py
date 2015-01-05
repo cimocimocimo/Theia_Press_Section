ae7bde03618fc9d8dad4b75495c1a94712852907
Aaron Cimolini <aaron@cimolini.com>
2015-01-05 04:11:10 -0500

    Finishes adding the settings for media and static files.

--- press_section/settings.py
+++ press_section/settings.py
@@ -1,4 +1,5 @@
 import os
+
 """
 Django settings for press_section project.
 
@@ -22,45 +23,44 @@ SECRET_KEY = '&k*63f5)n03s2)^z5)+k7b*2-sjy4w*m3po)j6wmm^ss9b7%re'
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
-
 TEMPLATE_DEBUG = False
 
+
+# allowed hosts
 ALLOWED_HOSTS = ['*']
 
 
 # Application definition
-
 ROOT_URLCONF = 'press_section.urls'
-
 WSGI_APPLICATION = 'press_section.wsgi.application'
 
 
 # Database
 # https://docs.djangoproject.com/en/1.7/ref/settings/#databases
-
+if 'RDS_DB_NAME' in os.environ:
+    DATABASES = {
+        'default': {
+            'ENGINE': 'django.db.backends.mysql',
+            'NAME': os.environ['RDS_DB_NAME'],
+            'USER': os.environ['RDS_USERNAME'],
+            'PASSWORD': os.environ['RDS_PASSWORD'],
+            'HOST': os.environ['RDS_HOSTNAME'],
+            'PORT': os.environ['RDS_PORT'],
+        }
+    }
 
 
 # Internationalization
 # https://docs.djangoproject.com/en/1.7/topics/i18n/
-
 LANGUAGE_CODE = 'en'
-
 TIME_ZONE = 'America/Chicago'
-
 USE_I18N = True
-
 USE_L10N = True
-
 USE_TZ = True
 
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/1.7/howto/static-files/
-
-MEDIA_URL = '/media/'
-MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
-STATIC_ROOT = os.path.join(BASE_DIR, 'static')
-
 STATICFILES_DIRS = (
     os.path.join(BASE_DIR, 'press_section', 'static'),
 )
@@ -72,8 +72,19 @@ STATICFILES_FINDERS = (
     'compressor.finders.CompressorFinder',
 )
 
+# Compressor Settings
+COMPRESS_ENABLED = True
+COMPRESS_OFFLINE = True
+
+COMPRESS_PRECOMPILERS = (
+    # ('text/scss', 'sass --scss {infile} {outfile}'),
+    ('text/scss', 'press_section.helpers.ScssFilter'),
+)
+# COMPRESS_CSS_FILTERS = [
+#     'compressor.filters.css_default.CssAbsoluteFilter'
+# ]
+
 # Django Storages
-DEFAULT_FILE_STORAGE = 'storages.backends.s3boto.S3BotoStorage'
 AWS_ACCESS_KEY_ID = os.environ['AWS_ACCESS_KEY_ID']
 AWS_SECRET_KEY = os.environ['AWS_SECRET_KEY']
 AWS_STORAGE_BUCKET_NAME = 'theia-press-section-assets'
@@ -82,22 +93,36 @@ S3_URL = 'https://%s.s3.amazonaws.com/' % AWS_STORAGE_BUCKET_NAME
 # Used to make sure that only changed files are uploaded with collectstatic
 AWS_PRELOAD_METADATA = True
 
+# remove the auth string from the generated static file urls
 AWS_QUERYSTRING_AUTH = False
 AWS_S3_SECURE_URLS = False
 
-COMPRESS_PRECOMPILERS = (
-    # ('text/scss', 'sass --scss {infile} {outfile}'),
-    ('text/scss', 'press_section.helpers.ScssFilter'),
-)
-COMPRESS_ENABLED = True
-COMPRESS_OFFLINE = True
-STATIC_URL = COMPRESS_URL = S3_URL
-COMPRES_ROOT = STATIC_ROOT
-STATICFILES_STORAGE = COMPRESS_STORAGE = 'press_section.storage.CachedS3BotoStorage'
 
-COMPRESS_CSS_FILTERS = [
-    'compressor.filters.css_default.CssAbsoluteFilter'
-]
+DEFAULT_FILE_STORAGE = 'press_section.storage.DefaultS3BotoStorage'
+STATICFILES_STORAGE = COMPRESS_STORAGE = 'press_section.storage.StaticS3BotoStorage'
+
+DEFAULT_S3_PATH = "media"
+STATIC_S3_PATH = "static"
+
+MEDIA_ROOT = '/%s/' % DEFAULT_S3_PATH
+MEDIA_URL = '//%s.s3.amazonaws.com/media/' % AWS_STORAGE_BUCKET_NAME
+
+# STATIC_ROOT = "/%s/" % STATIC_S3_PATH
+STATIC_ROOT = os.path.join(BASE_DIR, 'static')
+STATIC_URL = '//%s.s3.amazonaws.com/static/' % AWS_STORAGE_BUCKET_NAME
+
+COMPRESS_URL = STATIC_URL
+COMPRESS_ROOT = STATIC_ROOT
+
+ADMIN_MEDIA_PREFIX = STATIC_URL + 'admin/'
+
+# MEDIA_URL = '/media/'
+# MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
+# STATIC_ROOT = os.path.join(BASE_DIR, 'static')
+# STATIC_URL = COMPRESS_URL = S3_URL
+# COMPRES_ROOT = STATIC_ROOT
+
+
 
 SITE_ID = 1
 
@@ -191,8 +216,6 @@ INSTALLED_APPS = (
 )
 
 
-
-
 LANGUAGES = (
     ## Customize this
     ('en', gettext('en')),
@@ -233,18 +256,6 @@ CMS_TEMPLATES = (
 CMS_PERMISSION = True
 
 CMS_PLACEHOLDER_CONF = {}
-
-if 'RDS_DB_NAME' in os.environ:
-    DATABASES = {
-        'default': {
-            'ENGINE': 'django.db.backends.mysql',
-            'NAME': os.environ['RDS_DB_NAME'],
-            'USER': os.environ['RDS_USERNAME'],
-            'PASSWORD': os.environ['RDS_PASSWORD'],
-            'HOST': os.environ['RDS_HOSTNAME'],
-            'PORT': os.environ['RDS_PORT'],
-        }
-    }
 
 
 MIGRATION_MODULES = {
--- press_section/storage.py
+++ press_section/storage.py
@@ -2,33 +2,49 @@ from django.core.files.storage import get_storage_class
 from storages.backends.s3boto import S3BotoStorage
 import urllib
 import urlparse
+from django.conf import settings
 
-class CachedS3BotoStorage(S3BotoStorage):
-    """
-    S3 storage backend that saves the files locally, too.
-    """
 
-    querystring_auth=False
+
+class DefaultS3BotoStorage(S3BotoStorage):
+    def __init__(self, *args, **kwargs):
+        kwargs['location'] = settings.DEFAULT_S3_PATH
+        super(DefaultS3BotoStorage, self).__init__(*args, **kwargs)
+
+class StaticS3BotoStorage(S3BotoStorage):
+    """
+    Storage for static files.
+    The folder is defined in settings.STATIC_S3_PATH
+    Saves the files locally, too.
+    """
 
     def __init__(self, *args, **kwargs):
+        kwargs['location'] = settings.STATIC_S3_PATH
+        super(StaticS3BotoStorage, self).__init__(*args, **kwargs)
 
-        # merge in any arguments that were passed
-        # kwargs.update(dict(querystring_auth=False))
-
-        super(CachedS3BotoStorage, self).__init__(*args, **kwargs)
+        # define local storage for compressor files
         self.local_storage = get_storage_class(
             "compressor.storage.CompressorFileStorage")()
 
     def save(self, name, content):
-        name = super(CachedS3BotoStorage, self).save(name, content)
+        """
+        save locally for compressor to be able to compare files.
+        """
+        # call super save method
+        name = super(StaticS3BotoStorage, self).save(name, content)
+        # as well as saving locally. 
         self.local_storage._save(name, content)
         return name
 
     def url(self, name):
-        orig = super(CachedS3BotoStorage, self).url(name)
+        """
+        Removes security tokens from static file urls.
+        """
+        orig = super(StaticS3BotoStorage, self).url(name)
         scheme, netloc, path, params, query, fragment = urlparse.urlparse(orig)
         params = urlparse.parse_qs(query)
         if 'x-amz-security-token' in params:
             del params['x-amz-security-token']
         query = urllib.urlencode(params)
         return urlparse.urlunparse((scheme, netloc, path, params, query, fragment))
+
