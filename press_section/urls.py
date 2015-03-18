8fc9d51fc7785453ba41000ab284a5340c8cc8e6
Aaron Cimolini <aaron@cimolini.com>
2015-03-17 20:27:24 -0400

    formatting

--- press_section/urls.py
+++ press_section/urls.py
@@ -7,12 +7,12 @@ from django.conf import settings
 
 admin.autodiscover()
 
-urlpatterns = i18n_patterns('',
+urlpatterns = i18n_patterns(
+    '',
     url(r'^admin/', include(admin.site.urls)),  # NOQA
     url(r'^sitemap\.xml$', 'django.contrib.sitemaps.views.sitemap',
         {'sitemaps': {'cmspages': CMSSitemap}}),
-    url(r'^', include('cms.urls')),
-)
+    url(r'^', include('cms.urls')))
 
 # This is only needed when using runserver.
 if settings.DEBUG:
