391c00762921dfdb9209ee48ba89c3b3bdf0c340
Aaron Cimolini <aaron@cimolini.com>
2015-09-15 23:05:35 -0400

    reverse ordering for articles.

--- articles/views.py
+++ articles/views.py
@@ -9,7 +9,7 @@ from articles.models import Article
 def index(request, page_number=1):
 
     items_per_page = 8
-    query_set = Article.objects.order_by('original_publication_date')
+    query_set = Article.objects.order_by('-original_publication_date')
     paginator = Paginator(query_set, items_per_page)
     base_url = reverse('articles:index')
 
