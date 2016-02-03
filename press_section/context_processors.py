9938866ddd80d28eedae3852a59b47ce098054e8
Aaron Cimolini <aaron@cimolini.com>
2016-02-02 23:58:13 -0500

    Adds OpenGraph support to the celebrity detail page.

--- press_section/context_processors.py
+++ press_section/context_processors.py
@@ -1,4 +1,5 @@
 from django.core.urlresolvers import resolve
+from django.contrib.sites.models import Site
 
 def appname(request):
     try:
@@ -7,3 +8,7 @@ def appname(request):
         app_name = None
 
     return {'appname': app_name}
+
+def site_domain(request):
+    current_site = Site.objects.get_current()
+    return {'site_domain': current_site.domain}
