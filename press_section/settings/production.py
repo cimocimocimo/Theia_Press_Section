270f929b3aeab7fc744ee07aa72af402b30eee4a
Aaron Cimolini <aaron@cimolini.com>
2017-06-23 11:03:31 -0700

    deploy.

--- press_section/settings/production.py
+++ press_section/settings/production.py
@@ -17,7 +17,7 @@ ALLOWED_HOSTS = ['localhost',
 SHOPIFY_SHOP_NAME = 'theia'
 SHOPIFY_SHOP_URL = 'https://theiacouture.com/'
 SHOPIFY_SHOP_DOMAIN = 'theiacouture.com'
-SHOPIFY_ADMIN_URL = "https://%s:%s@%s.myshopify.com/admin" % (SHOPIFY_API_KEY, SHOPIFY_PASSWORD, SHOPIFY_SHOP_NAME)
+# SHOPIFY_ADMIN_URL = "https://%s:%s@%s.myshopify.com/admin" % (SHOPIFY_API_KEY, SHOPIFY_PASSWORD, SHOPIFY_SHOP_NAME)
 
 # Database
 # https://docs.djangoproject.com/en/1.7/ref/settings/#databases
